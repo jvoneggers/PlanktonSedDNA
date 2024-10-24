71dd6be833ca930d1bb290d0403c24861c0da6b7
Jordan Von Eggers <jordanvoneggers@gmail.com>
2024-10-24 14:47:04 -0600

    Update files

--- 1_LoadData/1_CreatePhyloseq_ZOTUexact_TAX80.R
+++ 1_LoadData/1_CreatePhyloseq_ZOTUexact_TAX80.R
@@ -6,8 +6,8 @@ require(tidyverse)
 metadata<-read.csv(list.files(pattern = "*_sample_data_dates.csv", full.names = TRUE), row.names=1, header=T)
 
 ### b. read in V7 files
-rarefied_esv_table <- read.csv("2_DataCleaning/V7/2024-06-19_18S_V7_ZOTUexact_tax80_Rarefied_ESV_table.csv", row.names=1, header=T)
-taxonomy <- read.csv("2_DataCleaning/V7/2024-06-19_18S_V7_ZOTUexact_tax80_tax_table_notnorm.csv", row.names=1, header=T)
+rarefied_esv_table <- read.csv("1_LoadData/2024-06-19_18S_V7_ZOTUexact_tax80_Rarefied_ESV_table.csv", row.names=1, header=T)
+taxonomy <- read.csv("1_LoadData/2024-06-19_18S_V7_ZOTUexact_tax80_tax_table_notnorm.csv", row.names=1, header=T)
 
 ### c. Put into Phyloseq
 #put in "Unassigned" in blanks in taxa tab
@@ -40,8 +40,8 @@ rm(samp_dat)
 
 
 ### d. read in V9 files
-rarefied_esv_table <- read.csv("2_DataCleaning/V9/2024-06-19_18S_V9_ZOTUexact_tax80_Rarefied_ESV_table.csv", row.names=1, header=T)
-taxonomy <- read.csv("2_DataCleaning/V9/2024-06-19_18S_V9_ZOTUexact_tax80_tax_table_notnorm.csv", row.names=1, header=T)
+rarefied_esv_table <- read.csv("1_LoadData/2024-06-19_18S_V9_ZOTUexact_tax80_Rarefied_ESV_table.csv", row.names=1, header=T)
+taxonomy <- read.csv("1_LoadData/2024-06-19_18S_V9_ZOTUexact_tax80_tax_table_notnorm.csv", row.names=1, header=T)
 
 ### e. put into Phyloseq
 #put in "Unassigned" in blanks in taxa tab
